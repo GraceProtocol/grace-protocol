ebf659665087f48578487f63d824614e7870eed3
nourharidy <nourharidy@yahoo.com>
2023-10-16 19:20:01 +0300

    reserve: add timelocked pull, safeTransfer

--- src/Reserve.sol
+++ src/Reserve.sol
@@ -11,24 +11,61 @@ interface IERC20 {
 
 contract Reserve {
 
+    struct PullRequest {
+        uint timestamp;
+        uint256[] amounts;
+        IERC20[] tokens;
+    }
+
     IERC20 public immutable grace;
+    address public owner;
     uint constant MANTISSA = 1e18;
-    uint locked = 1; // 1 = unlocked, 2 = locked
+    uint public locked = 1; // 1 = unlocked, 2 = locked
+    bytes4 private constant SELECTOR = bytes4(keccak256(bytes('transfer(address,uint256)')));
+    PullRequest public pullRequest;
 
     constructor(IERC20 _grace) {
         grace = _grace;
     }
 
-    function rageQuit(uint256 amount, IERC20[] memory tokens) external {
+    modifier onlyOwner {
+        require(msg.sender == owner, "onlyOwner");
+        _;
+    }
+
+    function _safeTransfer(address token, address to, uint value) private {
+        (bool success, bytes memory data) = token.call(abi.encodeWithSelector(SELECTOR, to, value));
+        require(success && (data.length == 0 || abi.decode(data, (bool))), 'Reserve: TRANSFER_FAILED');
+    }
+
+
+    function setOwner(address _owner) external onlyOwner { owner = _owner; }
+
+    function requestPull(IERC20[] calldata tokens, uint256[] calldata amounts) external onlyOwner {
+        require(tokens.length == amounts.length, "lengthMismatch");
+        pullRequest = PullRequest(block.timestamp, amounts, tokens);
+        emit PullRequested(msg.sender, amounts, tokens);
+    }
+
+    function executePull(address dst) external onlyOwner {
+        PullRequest memory request = pullRequest;
+        require(block.timestamp > request.timestamp + 90 days, "tooSoon");
+        pullRequest = PullRequest(0, new uint256[](0), new IERC20[](0));
+        for(uint i = 0; i < request.tokens.length; i++) {
+            _safeTransfer(address(request.tokens[i]), dst, request.amounts[i]);
+        }
+    }
+
+    function rageQuit(uint256 graceAmount, IERC20[] memory tokens) external {
         require(locked == 1, "locked");
         locked = 2;
         uint256 dayOfQuarter = (block.timestamp / 1 days) % 90;
         require(dayOfQuarter == 1, "Function can only be called on the first day of each quarter");
-        if(amount == type(uint256).max) amount = grace.balanceOf(msg.sender);
-        grace.transferFrom(msg.sender, address(this), amount);
+        if(graceAmount == type(uint256).max) graceAmount = grace.balanceOf(msg.sender);
+        grace.transferFrom(msg.sender, address(this), graceAmount);
         uint totalSupply = grace.totalSupply();
-        grace.burn(amount);
-        uint shareMantissa = amount * MANTISSA / totalSupply;
+        grace.burn(graceAmount);
+        uint shareMantissa = graceAmount * MANTISSA / totalSupply;
         for(uint i = 0; i < tokens.length; i++) {
             for(uint j = i + 1; j < tokens.length; j++) {
                 require(tokens[i] != tokens[j], "duplicate token");
@@ -36,9 +73,11 @@ contract Reserve {
             uint balance = tokens[i].balanceOf(address(this));
             require(balance > 0, "zeroBalance");
             uint share = balance * shareMantissa / MANTISSA;
-            tokens[i].transfer(msg.sender, share);
+            _safeTransfer(address(tokens[i]), msg.sender, share);
         }
         locked = 1;
     }
+
+    event PullRequested(address indexed sender, uint256[] amounts, IERC20[] tokens);
 
 }
