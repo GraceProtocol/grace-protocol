bf201c53cd4976ff7f8f417ac01a7a1379504a97
nourharidy <nourharidy@yahoo.com>
2024-02-25 01:48:11 +0200

    add VaultFactory tests

--- test/VaultFactory.t.sol
+++ test/VaultFactory.t.sol
@@ -1,91 +1,141 @@
-// // SPDX-License-Identifier: UNLICENSED
-// pragma solidity 0.8.22;
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.22;
 
-// import "forge-std/Test.sol";
-// import "../src/VaultFactory.sol";
-// import "../src/Vault.sol";
-// import "./mocks/ERC20.sol";
+import "forge-std/Test.sol";
+import "../src/VaultFactory.sol";
+import "../src/Vault.sol";
+import "./mocks/ERC20.sol";
 
-// contract MockPool is ERC20 {
+
+contract MockPool is ERC20 {
     
-//     ERC20 public asset;
+    ERC20 public asset;
 
-//     constructor() {
-//         asset = new ERC20();
-//     }
-// }
+    constructor() {
+        asset = new ERC20();
+    }
+}
 
-// contract VaultFactoryTest is Test {
+contract MockGTR is ERC20 {
+    function minters(address) external view returns (uint) {
+        return type(uint).max;
+    }
+}
 
-//     ERC20 public gtr;
-//     VaultFactory vaultFactory;
+contract VaultFactoryTest is Test {
 
-//     function setUp() public {
-//         gtr = new ERC20();
-//         vaultFactory = new VaultFactory(address(gtr), address(0x1));
-//     }
+    MockGTR public gtr;
+    VaultFactory vaultFactory;
 
-//     function test_constructor() public {
-//         assertEq(address(vaultFactory.gtr()), address(gtr));
-//         assertEq(vaultFactory.operator(), address(this));
-//     }
+    function setUp() public {
+        gtr = new MockGTR();
+        vaultFactory = new VaultFactory(address(gtr), address(0x1), 1000);
+    }
 
-//     function test_createVault() public {
+    function test_constructor() public {
+        assertEq(address(vaultFactory.gtr()), address(gtr));
+        assertEq(vaultFactory.weth(), address(0x1));
+        assertEq(vaultFactory.operator(), address(this));
+        assertEq(vaultFactory.rewardBudget(), 1000);
+    }
 
-//         uint initialRewardBudget = 1000;
+    function test_createVault() public {
+        MockPool pool = new MockPool();
 
-//         MockPool pool = new MockPool();
+        vm.startPrank(address(1));
+        vm.expectRevert("onlyOperator"); // not operator
+        Vault(payable(vaultFactory.createVault(
+            address(pool),
+            1
+        )));
+        vm.stopPrank();
 
-//         Vault vault = Vault(payable(vaultFactory.createVault(
-//             address(pool),
-//             initialRewardBudget
-//         )));
+        Vault vault = Vault(payable(vaultFactory.createVault(
+            address(pool),
+            1
+        )));
 
-//         assertEq(vaultFactory.allVaultsLength(), 1);
-//         assertEq(vaultFactory.allVaults(0), address(vault));
+        assertEq(address(vault.pool()), address(pool));
+        assertEq(vaultFactory.isVault(address(vault)), true);
+        assertEq(vaultFactory.balanceOf(address(vault)), 1);
+        assertEq(vaultFactory.totalSupply(), 1);
+        assertEq(vaultFactory.allVaultsLength(), 1);
+        assertEq(vaultFactory.allVaults(0), address(vault));
+    }
 
-//         assertEq(address(vault.pool()), address(pool));
-//         assertEq(vault.rewardBudget(), initialRewardBudget);
-//     }
+    function test_setOperator() public {
+        vaultFactory.setOperator(address(1));
+        assertEq(vaultFactory.operator(), address(1));
 
-//     function test_setOperator() public {
-//         vaultFactory.setOperator(address(1));
-//         assertEq(vaultFactory.operator(), address(1));
+        vm.expectRevert("onlyOperator"); // no longer operator
+        vaultFactory.setOperator(address(0x2));
+    }
 
-//         vm.expectRevert("onlyOperator"); // no longer operator
-//         vaultFactory.setOperator(address(0x2));
-//     }
+    function test_setWeight() public {
+        MockPool pool = new MockPool();
+        Vault vault = Vault(payable(vaultFactory.createVault(
+            address(pool),
+            1
+        )));
+        assertEq(vaultFactory.balanceOf(address(vault)), 1);
 
-//     function test_transferReward() public {
-//         MockPool pool = new MockPool();
-//         address vault = vaultFactory.createVault(
-//             address(pool),
-//             1000
-//         );
+        vaultFactory.setWeight(address(vault), 2);
+        assertEq(vaultFactory.balanceOf(address(vault)), 2);
 
-//         vm.expectRevert("onlyVault"); // not vault
-//         vaultFactory.transferReward(address(1), 100);
-//         vm.prank(vault);
-//         vaultFactory.transferReward(address(1), 100);
-//         assertEq(gtr.balanceOf(address(1)), 100);
-//     }
+        vm.startPrank(address(1));
+        vm.expectRevert("onlyOperator"); // not operator
+        vaultFactory.setWeight(address(vault), 3);
+        vm.stopPrank();
+    }
 
-//     function test_setBudget() public {
-//         MockPool pool = new MockPool();
-//         address payable vault = payable(vaultFactory.createVault(
-//             address(pool),
-//             1000
-//         ));
+    function test_claim() public {
+        MockPool pool = new MockPool();
+        Vault vault = Vault(payable(vaultFactory.createVault(
+            address(pool),
+            1
+        )));
 
-//         vm.startPrank(address(1));
-//         vm.expectRevert("onlyOperator"); // not operator
-//         vaultFactory.setBudget(vault, 100);
-//         vm.stopPrank();
-//         assertEq(Vault(vault).rewardBudget(), 1000);
-//         vaultFactory.setBudget(vault, 100);
-//         assertEq(Vault(vault).rewardBudget(), 100);
-//         vm.expectRevert("onlyVault"); // not vault
-//         vaultFactory.setBudget(address(1), 100);
-//     }
+        skip(365 days);
+        vm.prank(address(vault));
+        vaultFactory.claim();
+        assertEq(gtr.balanceOf(address(vault)), 1000);
+    }
 
-// }
+    function test_claimable() public {
+        MockPool pool = new MockPool();
+        Vault vault = Vault(payable(vaultFactory.createVault(
+            address(pool),
+            1
+        )));
+
+        assertEq(vaultFactory.claimable(address(vault)), 0);
+
+        skip(365 days);
+        assertEq(vaultFactory.claimable(address(vault)), 1000);
+
+        vm.prank(address(vault));
+        vaultFactory.claim();
+        assertEq(vaultFactory.claimable(address(vault)), 0);        
+    }
+
+    function test_setBudget() public {
+
+        MockPool pool = new MockPool();
+        Vault vault = Vault(payable(vaultFactory.createVault(
+            address(pool),
+            1
+        )));
+
+        vm.startPrank(address(1));
+        vm.expectRevert("onlyOperator"); // not operator
+        vaultFactory.setBudget(2000);
+        vm.stopPrank();
+
+        vaultFactory.setBudget(2000);
+        assertEq(vaultFactory.rewardBudget(), 2000);
+
+        skip(365 days);
+        assertEq(vaultFactory.claimable(address(vault)), 2000);
+    }
+
+}
