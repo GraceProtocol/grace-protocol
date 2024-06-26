ecdbc3a3c80e6325e73c7cc8ca5471b854bd5018
nourharidy <nourharidy@yahoo.com>
2024-06-27 02:15:03 +0300

    ethereum deployment

--- sh/deploy/mainnet.sh
+++ sh/deploy/mainnet.sh
@@ -4,4 +4,4 @@
 source .env
 
 # To deploy and verify our contract
-forge script script/deploy/MainnetDeployer.s.sol --rpc-url $MAINNET_RPC_URL --broadcast -vvvv --etherscan-api-key $ETHERSCAN_API_KEY --verify
+forge script script/deploy/MainnetDeployer.s.sol --rpc-url $MAINNET_RPC_URL --broadcast -vvvv --etherscan-api-key $ETHERSCAN_API_KEY --verify --slow
